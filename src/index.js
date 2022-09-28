d6fe2330b6fa99e6fcaca6b01bf00c2cd8f83a69
Victor Caio <kaioaraujo426@gmail.com>
2022-09-28 13:25:01 -0300

    feat: add get all and todos endpoints

--- src/index.js
+++ src/index.js
@@ -8,14 +8,22 @@ const users = [];
 
 function checkValidAccount(req, res, next) {
   const { username } = req.body;
-  const searchUsername = users.find((users) => users.username === username);
+  const searchUsername = users.some((users) => users.username === username);
   if (searchUsername)
     return res.status(400).send({ message: 'Username exists' });
 
-  next();
+  return next();
 }
 
-function checkExistsUserAccount(req, res, next) {}
+function checkExistsUserAccount(req, res, next) {
+  const { username } = req.headers;
+  const searchUsername = users.find((users) => users.username === username);
+  if (!searchUsername) {
+    return res.status(400).send({ error: 'Account not found' });
+  }
+  req.searchUsername = searchUsername;
+  return next();
+}
 
 app.use(cors());
 app.use(express.json());
@@ -27,7 +35,12 @@ app.get('/getAll', (req, res) => {
 app.post('/users', checkValidAccount, (req, res) => {
   const { name, username } = req.body;
   users.push({ _id: uuidv4(), name, username, todos: [] });
-  res.status(201).send();
+  return res.status(201).send();
+});
+
+app.get('/todos', checkExistsUserAccount, (req, res) => {
+  const { searchUsername } = req;
+  return res.send(searchUsername.todos);
 });
 
 module.exports = app;
