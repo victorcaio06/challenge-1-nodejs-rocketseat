ef8a5aa87af41fb53c6d47312fb62f011f0341b5
Victor Caio <kaioaraujo426@gmail.com>
2022-09-28 10:07:30 -0300

    feat: add create users endpoint

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+const users = [];
+
+function checkValidAccount(req, res, next) {
+  const { username } = req.body;
+  const searchUsername = users.find((users) => users.username === username);
+  if (searchUsername)
+    return res.status(400).send({ message: 'Username exists' });
+
+  next();
+}
+
+function checkExistsUserAccount(req, res, next) {}
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.post('/users', checkValidAccount, (req, res) => {
+  const { name, username } = req.body;
+  users.push({ _id: uuidv4(), name, username, todos: [] });
+  res.status(201).send();
+});
+
+module.exports = app;
